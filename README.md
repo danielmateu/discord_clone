d00f9e7e2fab72378acf00e3b69612e93875950c
Daniel Mateu <danielmateu86@gmail.com>
2023-09-18 15:29:57 +0200

    Dork mode implmented

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 1. Tailwind
 2. Shadcn/ui
 3. Clerk 
+4. Next-Themes
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 
   return (
-    <div className="">
+    <div className="flex gap-2">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
