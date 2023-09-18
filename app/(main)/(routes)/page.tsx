0da24df0fc154431d2a63edb7ccc9088e14e438d
Daniel Mateu <danielmateu86@gmail.com>
2023-09-18 14:42:26 +0200

    Protected route and pubic route created

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+
+  return (
+    <div className="">
+      <h1 className=''>Hello Discord</h1>
+      <p>Esto es una ruta protegida</p>
+      <Button >Button</Button>
+
+    </div>
+  )
+}
