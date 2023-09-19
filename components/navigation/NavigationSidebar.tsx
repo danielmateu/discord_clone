8b5c7b4d61542fcd83bc248491a92234141a34cf
Daniel Mateu <danielmateu86@gmail.com>
2023-09-19 12:49:39 +0200

    Navigation Sidebar implementada

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,7 +1,13 @@
+import { redirect } from "next/navigation"
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
-import { redirect } from "next/navigation"
 import { NavigationAction } from "./NavigationAction"
+import { NavigationItem } from "./NavigationItem"
+import { ModeToggle } from "../mode-toggle"
+import { UserButton } from "@clerk/nextjs"
 
 
 export const NavigationSidebar = async () => {
@@ -24,7 +30,31 @@ export const NavigationSidebar = async () => {
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22]">
             <NavigationAction />
-
+            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div
+                        key={server.id}
+                    >
+                        <NavigationItem
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+            <div className="pb-3 mt-auto flex flex-col items-center gap-y-4">
+                <ModeToggle />
+                <UserButton
+                    afterSignOutUrl='/'
+                    appearance={{
+                        elements: {
+                            avatarBox: 'h-[40px] w-[40px] hover:scale-105 transition-all'
+                        }
+                    }}
+                />
+            </div>
         </div>
     )
 }
