4c8d132e0963361d5ca1cf0a332b3c67da6efe9d
Daniel Mateu <danielmateu86@gmail.com>
2023-09-20 12:12:40 +0200

    updating a server record to delete a member

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -25,6 +25,8 @@ export async function PATCH(
             return new NextResponse('Member ID Missing', { status: 400 })
         }
 
+        /* The code `const server = await db.server.update({...})` is updating a server record in the
+           database. */
         const server = await db.server.update({
             where: {
                 id: serverId,
@@ -61,6 +63,65 @@ export async function PATCH(
 
     } catch (error) {
         console.log("[MEMBER_ID_PATCH]", error)
+        return new NextResponse('Internal Error', { status: 500 })
+    }
+}
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { memberId: string } }
+) {
+    try {
+        const profile = await currentProfile()
+        const { searchParams } = new URL(req.url)
+
+        const serverId = searchParams.get('serverId')
+
+        if (!profile) {
+            return new NextResponse('Unauthorized', { status: 401 })
+        }
+
+        if (!serverId) {
+            return new NextResponse('Server ID Missing', { status: 400 })
+        }
+
+        if (!params.memberId) {
+            return new NextResponse('Member ID Missing', { status: 400 })
+        }
+
+        /* The code `const server = await db.server.update({...})` is updating a server record in the
+              database. */
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id
+            },
+            data: {
+                members: {
+                    deleteMany: {
+                        id: params.memberId,
+                        profileId: {
+                            not: profile.id
+                        }
+                    }
+                }
+            },
+            include: {
+                members: {
+                    include: {
+                        profile: true
+                    },
+                    orderBy: {
+                        role: 'asc'
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server)
+
+    } catch (error) {
+        console.log('[MEMBER_ID_DELETE]', error)
         return new NextResponse('Internal Error', { status: 500 })
     }
 }
